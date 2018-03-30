d345299dc2cdd13e4b16b77c313c827521d1a410
stop2stare <emitremmus0235@163.com>
2018-03-30 15:02:15 +0800

    add all types of escaped html

---
+++ index.js
@@ -0,0 +1,15 @@
+
+/**
+ * this version converts all the Dec/Hex/Entity type
+ * @param  {String} raw
+ * @return {String}
+ */
+module.exports = function(raw) {
+  return String(raw)
+    .replace(/&#34;|&#x22;|&quot;/gi, '"')
+    .replace(/&#38;|&#x26;|&amp;/gi, '&')
+    .replace(/&#39;|&#x27;/gi, '\'')
+    .replace(/&#58;|&#x3a;/gi, ':')
+    .replace(/&#60;|&#x3c;|&lt;/gi, '<')
+    .replace(/&#62;|&#x3e;|&gt;/gi, '>')
+}
